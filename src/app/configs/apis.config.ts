7878975d7792453e90e60c28b41278bad5eded35
Bailey Maynard <bailey_maynard@live.com>
2024-11-07 11:35:57 -0500

    updating backend url

--- src/app/configs/apis.config.ts
+++ src/app/configs/apis.config.ts
@@ -1,7 +1,8 @@
 export const apiConfig = {
 
     //database apis
-    databaseRootPath: "http://localhost:3000",
+    // databaseRootPath: "http://localhost:3000",
+    databaseRootPath: "https://budget-app-backend-xdje.onrender.com",
 
     // bils
     getBills: "/bills",
