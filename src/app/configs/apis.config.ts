87ab14ad42821cdcefabb536f58c8b76cdbe64dd
Bailey Maynard <bailey_maynard@live.com>
2024-12-22 14:02:05 -0500

    updating layout with new table

--- src/app/configs/apis.config.ts
+++ src/app/configs/apis.config.ts
@@ -1,8 +1,8 @@
 export const apiConfig = {
 
     //database apis
-    databaseRootPath: "http://localhost:3000",
-    // databaseRootPath: "https://budget-app-backend-xdje.onrender.com",
+    // databaseRootPath: "http://localhost:3000",
+    databaseRootPath: "https://budget-app-backend-xdje.onrender.com",
 
     // bils
     getBills: "/bills",
