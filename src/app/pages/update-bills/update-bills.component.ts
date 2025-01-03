37e42d7874483147fcd704d69ba3829686246754
Bailey Maynard <bailey_maynard@live.com>
2025-01-02 23:51:52 -0500

    updating update bill page

--- src/app/pages/update-bills/update-bills.component.ts
+++ src/app/pages/update-bills/update-bills.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren, ViewContainerRef } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { BillsService } from '../../services/bills.service';
 import { sessionConfig } from '../../configs/session.config';
 import { CodeUtil } from '../../services/code-util.service';
 import { ButtonModule } from 'primeng/button';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CommonModule, KeyValue } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SpinnerService } from '../../services/spinner.service';
 
 @Component({
   selector: 'app-update-bills',
@@ -14,19 +15,29 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     TableModule,
-    ButtonModule
+    ButtonModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './update-bills.component.html',
   styleUrl: './update-bills.component.scss'
 })
-export class UpdateBillsComponent implements OnInit {
+export class UpdateBillsComponent implements OnInit, AfterViewInit {
+
+  @ViewChildren('billInput') billInputs: QueryList<ElementRef> | undefined;
 
   bills: any;
+  billInputsDic: any[] = [];
   authToken: string | null = null;
+
+  newBillName: string | undefined;
+  newBillPrice: number | undefined;
+  showNewBillDialog: boolean = false;
 
   constructor(
     private billService: BillsService,
-    private codeUtil: CodeUtil
+    private codeUtil: CodeUtil,
+    private spinnerService: SpinnerService
   ) {}
 
   ngOnInit(): void {
@@ -38,6 +49,23 @@ export class UpdateBillsComponent implements OnInit {
     }
   }
 
+  ngAfterViewInit(): void {
+    this.spinnerService.showSpinner();
+    setTimeout(() => {
+      this.billInputs?.forEach((element) => {
+        let currentInputObj = {
+          id: element.nativeElement?.attributes?.id?.value,
+          element: element
+        };
+
+        this.billInputsDic.push(currentInputObj);
+      });
+
+      console.log(this.billInputsDic);
+      this.spinnerService.hideSpinner();
+    }, 2000);
+  }
+
   save(bill: any) {
     console.log(bill);
     if (this.authToken) {
@@ -46,5 +74,18 @@ export class UpdateBillsComponent implements OnInit {
       });
     }
     
+  }
+
+  addNewBill() {
+    let newBill = {
+      name: this.newBillName,
+      price: this.newBillPrice
+    };
+
+    console.log(newBill);
+  }
+
+  showNewBill() {
+    this.showNewBillDialog = !this.showNewBillDialog;
   }
 }
